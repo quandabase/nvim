b52a530eacd09b7402d05940d8aa836ef12b7ef3
quandabase <quandabase@gmail.com>
2024-01-28 09:11:33 +0700

    wip

--- init.lua
+++ init.lua
@@ -75,7 +75,6 @@ require('lazy').setup({
     'NLKNguyen/papercolor-theme',
     config = function()
       vim.cmd([[colorscheme PaperColor]])
-      vim.cmd([[set background=light]])
     end
   },
 
